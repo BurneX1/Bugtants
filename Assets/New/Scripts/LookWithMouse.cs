291de0f38a57b30b12973ac88f947af243bec02e
Ralph Maura <78460880+SharpWalker14@users.noreply.github.com>
2022-04-29 03:22:47 -0500

    Camera Angle

--- Assets/New/Scripts/LookWithMouse.cs
+++ Assets/New/Scripts/LookWithMouse.cs
@@ -57,7 +57,7 @@ public class LookWithMouse : MonoBehaviour
 #endif
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -80f, 90f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
