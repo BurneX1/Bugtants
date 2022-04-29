17982da5e4968e1cd4872989d5c5120ed6c9d4ac
Alejandro Medina <almpratto@gmail.com>
2022-04-29 05:42:24 -0500

    Player view & addition of player view n controller to playerprefab

--- Assets/New/Scripts/Player/PlayerController.cs
+++ Assets/New/Scripts/Player/PlayerController.cs
@@ -6,11 +6,17 @@ public class PlayerController : MonoBehaviour
 {
     private InputSystemActions inputStm;
     private FrontRayCaster c_ray;
+    [HideInInspector]
+    public Life c_life;
+    [HideInInspector]
+    public MP_System c_mp;
 
     void Awake()
     {
         //Component Settup -----------//
         c_ray = gameObject.GetComponent<FrontRayCaster>();
+        c_life = gameObject.GetComponent<Life>();
+        c_mp = gameObject.GetComponent<MP_System>();
         //-------------------------<<<//
 
 
@@ -18,6 +24,8 @@ public class PlayerController : MonoBehaviour
         inputStm = new InputSystemActions();
 
         inputStm.GamePlay.Interact.performed += _ => c_ray.Interact();
+        inputStm.GamePlay.Interact.performed += _ => Debug.Log("KeyPress");
+        inputStm.GamePlay.Crouch.performed += ctx => Debug.Log(ctx);
         //-------------------------<<<//
 
     }
