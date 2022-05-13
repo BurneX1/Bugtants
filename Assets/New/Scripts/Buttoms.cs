b7e03d290c24086e1c657a472005ac7b1e420f27
Ripheon <srchkgames@gmail.com>
2022-05-13 09:55:13 -0500

    Menu Advance

--- Assets/New/Scripts/Buttoms.cs
+++ Assets/New/Scripts/Buttoms.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Buttoms : MonoBehaviour
 {
@@ -15,5 +16,14 @@ public class Buttoms : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void ActivateCanvas(string canvasName)
+    {
+        GameObject.Find(canvasName).GetComponent<Canvas>().enabled = true;
+    }
+    public void DeActivateCanvas(string canvasName)
+    {
+        GameObject.Find(canvasName).GetComponent<Canvas>().enabled = false;
     }
 }
