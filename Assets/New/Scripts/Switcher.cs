a55657474c9dc2a968ddeea4f4d4a38ed96cb935
giussepels <giussepe_1999_1@hotmail.com>
2022-05-26 15:05:01 -0500

    LVL1_interactive

--- Assets/New/Scripts/Switcher.cs
+++ Assets/New/Scripts/Switcher.cs
@@ -8,6 +8,10 @@ public class Switcher : MonoBehaviour
     public bool binary;
     [HideInInspector]
     public bool activated;
+
+    public Light[] LightColorChange;
+
+    public GameObject[] Status_0, Status_1;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +22,17 @@ public class Switcher : MonoBehaviour
         if (!activated)
         {
             activated = true;
+            for (int i = 0; i < Status_0.Length; i++)
+            { Status_0[i].SetActive(false); ; }
+
+            for (int i = 0; i < Status_1.Length; i++)
+            { Status_1[i].SetActive(true); }
+
+            if(LightColorChange.Length!= 0)
+            {
+                for (int i = 0; i < LightColorChange.Length; i++)
+                { LightColorChange[i].color = Color.green; }
+            }
             Debug.Log("A");
         }
         else if (activated && binary)
