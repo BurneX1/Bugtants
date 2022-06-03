9c47c6454c5b7fa5a0e50b04d405e1c8893cccc3
Alejandro Medina <almpratto@gmail.com>
2022-06-03 09:08:12 -0500

    Avance UI data editing(forOptions&Debugging)

--- Assets/New/Scripts/ScriptTools/UITools/SlideDynValue.cs
+++ Assets/New/Scripts/ScriptTools/UITools/SlideDynValue.cs
@@ -6,18 +6,43 @@ using UnityEngine.UI;
 public class SlideDynValue : MonoBehaviour
 {
     public Slider sld;
-    public void ChnglideValue(float val)
+    public InputField inpt;
+    public void ChngSlideValue(float val)
     {
         if(sld != null)
         {
             sld.value = val;
         }
     }
-    public void ChnglideValue(string val)
+    public void ChngSlideValue(string val)
     {
         if (sld != null)
         {
             sld.value = float.Parse(val);
+        }
+    }
+
+    public void ChngInptValue(float val)
+    {
+        if (sld != null)
+        {
+            inpt.text = val + "";
+        }
+    }
+    public void ChngInptValue(string val)
+    {
+        if (sld != null)
+        {
+            inpt.text = val;
+            
+        }
+    }
+
+    public void EqualizeValues()
+    {
+        if(sld.value != float.Parse(inpt.text))
+        {
+            inpt.text = sld.value + "";
         }
     }
 }
