76039402c007da868bffa7bea79d955c017de9b6
Ralph Maura <78460880+SharpWalker14@users.noreply.github.com>
2022-07-05 02:15:41 -0500

    Tags of music and example of a SndCaping Volume

---
+++ Assets/New/Scripts/SoundsCapingVolume.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource),typeof(VolumeValue))]
+public class SoundsCapingVolume : MonoBehaviour
+{
+    private AudioSource audSrc;
+    private VolumeValue volumeScript;
+    [Range(0, 1)]
+    public float capVolume;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        audSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SoundsCapingSounder();
+    }
+
+    void SoundsCapingSounder()
+    {
+        if (audSrc.volume != volumeScript.volValue * capVolume)
+        audSrc.volume = volumeScript.volValue * capVolume;
+    }
+}
