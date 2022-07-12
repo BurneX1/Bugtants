63ab048f57145cc45b23d503fb33c4e93faa30bc
Ralph Maura <78460880+SharpWalker14@users.noreply.github.com>
2022-07-11 20:48:54 -0500

    Player event sounds and pause on 2 sections

--- Assets/New/Scripts/EventSoundSeen.cs
+++ Assets/New/Scripts/EventSoundSeen.cs
@@ -7,11 +7,15 @@ public class EventSoundSeen : MonoBehaviour
     public AudioClip clippie;
     private AudioSource whisper;
     public Detecter soundZone;
+    private VolumeValue volumeScript;
+    [Range(0, 1)]
+    public float capVolume;
     // Start is called before the first frame update
     void Start()
     {
         GameObject obj = GameObject.Find("Whisperer");
         whisper = obj.GetComponent<AudioSource>();
+        volumeScript = obj.GetComponent<VolumeValue>();
     }
 
     // Update is called once per frame
@@ -22,9 +26,12 @@ public class EventSoundSeen : MonoBehaviour
 
     void ZoneDetecter()
     {
+        if (whisper.volume != volumeScript.volValue * capVolume)
+            whisper.volume = volumeScript.volValue * capVolume;
         if (soundZone.touch)
         {
             whisper.clip = clippie;
+
             whisper.Play();
             Destroy(gameObject);
         }
