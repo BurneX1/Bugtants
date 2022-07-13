9061e763e3dacc1beb2afb6e396073a6c35e4566
Ripheon <69824045+Ripheon@users.noreply.github.com>
2022-07-13 16:50:08 -0500

    CinematicsCorrection

--- Assets/New/Scripts/CristhianTestScripts/SceneSimpleChange.cs
+++ Assets/New/Scripts/CristhianTestScripts/SceneSimpleChange.cs
@@ -5,6 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class SceneSimpleChange : MonoBehaviour
 {
+    [SerializeField]
+    private float timer;
+    public string sceneX;
+    public float maxTimer;
+    public bool autAct;
+
+    public void Update()
+    {
+
+            timer += Time.deltaTime;
+            if(timer >= maxTimer)
+            {
+                SceneManager.LoadScene(sceneX);
+            }
+    }
+
+
     public void SceneChange(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
